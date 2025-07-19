e7a81f944e04152840f31e5d5074dd939190fdb4
Oybek Nematov <1377oybeknematov1377@gmail.com>
2025-07-19 20:44:07 +0500

    Add project files.

---
+++ WebApplication4/Controllers/ExanController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("api/tools")]
+    public class ExanController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return new Random().Next(0, 101);
+        }
+
+        [HttpGet("greet")]
+        public async Task<string> Greet(string name)
+        {
+            await Task.Delay(2000);
+            return $"Salom, {name}";
+        }
+
+        [HttpGet("today")]
+
+        public string Today()
+        {
+            return DateTime.Today.ToString("yyyy-MM-dd ");
+        }
+
+
+        [HttpGet("length")]
+        public int Length(string text)
+        {
+            return text.Length;
+        }
+
+        [HttpGet("sqrt")]
+
+        public double sqrt(double value)
+        {
+            return Math.Sqrt(value);
+        }
+
+    }
+
+
+}
+
